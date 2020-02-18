602a4e5847282ccae6d76e308ab345cac2e39534
MD. Tanjil Hasan <engineertanjil@gmail.com>
2020-02-18 13:20:21 +0600

    vuex store empty

--- public/js/app.js
+++ public/js/app.js
@@ -67426,8 +67426,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\taskola\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\taskola\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\LaravelVueBoilerPlate\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\LaravelVueBoilerPlate\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/js/Store/store.js
+++ resources/js/Store/store.js
@@ -5,11 +5,6 @@ Vue.use(Vuex);
 
 new Vuex.Store({
 	state:{
-		filters:{
-			itemPerPage: 10,
-            search: '',
-            column: 'id',
-            dir: 'desc',
-		}
+		
 	}
 });
